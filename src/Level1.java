2971eb81ec65c5bfe7d87d628e27372d500c0e80
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-13 14:55:28 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,2 +1,21 @@
 public class Level1 {
+    public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
+        if (h2 > h1 || w2 > w1) {
+            return false;
+        }
+
+        String[] str = s2.split(" ");
+
+        for (int i = 0; i < str.length; i++) {
+            if (!s1.contains(str[i])) {
+                break;
+            }
+
+            if (i == str.length - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
