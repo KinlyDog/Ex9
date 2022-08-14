2bc97a16cb034f691e856b2f5820ba7794b09768
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-14 21:53:19 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,34 +1,32 @@
 public class Level1 {
-      public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
+    public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
         String[] str1 = s1.split(" ");
         String[] str2 = s2.split(" ");
 
-        int str = h1 - h2 + 1;
-        int stlb = w1 - w2 + 1;
+        int difStr = h1 - h2 + 1;
+        int difCol = w1 - w2 + 1;
 
         int x = 0;
         int y = 0;
-        int i = 0;
-        int j = 0;
 
-        while (x != str) {
+        while (x != difStr) {
+            boolean flag = true;
 
-            for (i = 0; i < h2; i++) {
-                for (j = 0; j < w2; j++) {
+            for (int i = 0; i < h2; i++) {
+                for (int j = 0; j < w2; j++) {
                     if (str2[i].charAt(j) != str1[i + x].charAt(j + y)) {
                         y++;
 
-                        i = h2;
-                        j = w2;
+                        flag = false;
+                        break;
                     }
                 }
+                if (!flag) break;
             }
 
-            if (i == h2 && j == w2) {
-                return true;
-            }
+            if (flag) return true;
 
-            if (y == stlb) {
+            if (y == difCol) {
                 y = 0;
                 x++;
             }
