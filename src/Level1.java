70b4b35b642568cf9f2b9cf494d26cbc15949b53
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-14 18:02:20 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,21 +1,10 @@
 public class Level1 {
-    public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
-        if (h2 > h1 || w2 > w1) {
-            return false;
-        }
-
-        String[] str1 = s1.split(" ");
-        String[] str2 = s2.split(" ");
-
-        String[] str = new String[str2.length];
-
+    public static boolean contSearch(String[] str1, String[] str2) {
         int x = 0;
         int y = 0;
-        int tmp = -1;
 
         while (x < str1.length) {
-            if (str1[x].contains(str2[y]) && y < str2.length) {
-                str[y] = str1[x];
+            if (str1[x].contains(str2[y])) {
                 y++;
                 x++;
 
@@ -25,65 +14,42 @@ public class Level1 {
             }
 
             if (y == str2.length) {
-                tmp = x - y;
-                break;
+                return true;
             }
         }
-
-        if (tmp < 0) return false;
-
-        String[] strRev = new String[str[0].length()];
-        String[] str2Rev = new String[str2[0].length()];
-
-        for (int i = 0; i < strRev.length; i++) {
-            strRev[i] = "";
-        }
-        for (int i = 0; i < str2Rev.length; i++) {
-            str2Rev[i] = "";
-        }
-
-        for (int i = 0; i < strRev.length; i++) {
-            int j = 0;
-            while (j < str.length) {
-                strRev[i] += str[j].charAt(i);
-                j++;
-            }
-        }
-
-        for (int i = 0; i < str2Rev.length; i++) {
-            int j = 0;
-            while (j < str2.length) {
-                str2Rev[i] += str2[j].charAt(i);
-                j++;
-            }
-        }
-
-        x = 0;
-        y = 0;
-        tmp = -1;
-
-        while (x < strRev.length) {
-            if (strRev[x].contains(str2Rev[y]) && y < str2Rev.length) {
-                y++;
-                x++;
-
-            } else {
-                x = x - y + 1;
-                y = 0;
-            }
-
-            if (y == str2Rev.length) {
-                tmp = x - y;
-                break;
-            }
-        }
-
-        if (tmp > 0) return true;
 
         return false;
     }
 
-    public static void main(String[] args) {
-        System.out.println(TankRush(3,3, "321 694 798", 2, 2, "69 98"));
+    public static String[] revers(String[] str) {
+        String[] rev = new String[str[0].length()];
+
+        for (int i = 0; i < rev.length; i++) {
+            rev[i] = "";
+        }
+
+        for (int i = 0; i < rev.length; i++) {
+            for (int j = 0; j < str.length; j++) {
+                rev[i] += str[j].charAt(i);
+            }
+        }
+
+        return rev;
+    }
+    public static boolean TankRush(int h1, int w1, String s1, int h2, int w2, String s2) {
+        String[] str1 = s1.split(" ");
+        String[] str2 = s2.split(" ");
+
+        String[] strRev1 = revers(str1);
+        String[] strRev2 = revers(str2);
+
+        boolean t1 = contSearch(str1, str2);
+        boolean t2 = contSearch(strRev1, strRev2);
+
+        if (t1 && t2) {
+            return true;
+        }
+
+        return false;
     }
 }
